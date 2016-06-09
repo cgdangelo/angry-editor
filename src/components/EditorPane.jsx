bd935f7fde242ace19dcee63e6089688bae53794
Charles D'Angelo <c.g.dangelo@gmail.com>
2016-06-09 16:50:39 -0400

    Add simple layout

--- src/components/EditorPane.jsx
+++ src/components/EditorPane.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './EditorPane.css';
 
-export default () => (
-  <div>
-    <textarea className={styles.textarea}></textarea>
-    <textarea className={styles.textarea}></textarea>
-  </div>
-);
+export default class EditorPane extends Component {
+  constructor() {
+    super();
+
+    this.handleInput = ::this.handleInput;
+  }
+
+  state = {
+    input: '',
+    output: '',
+  };
+
+  handleInput({ target: { value } }) {
+    this.setState({
+      input: value,
+      output: value.length,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <textarea
+          className={styles.textarea}
+          onChange={this.handleInput}
+          value={this.state.input}
+        />
+
+        <textarea
+          disabled
+          className={styles.textarea}
+          value={this.state.output}
+        />
+      </div>
+    );
+  }
+}
