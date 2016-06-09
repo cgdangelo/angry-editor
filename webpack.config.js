78db406976c5ba3bf2a72b1af3e03219c926b8f6
Charles D'Angelo <c.g.dangelo@gmail.com>
2016-06-09 16:11:39 -0400

    Add babel and react

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
+const srcPath = path.join(__dirname, 'src');
+const buildPath = path.join(__dirname, 'build');
+
 module.exports = {
-  entry: {
-    app: './src'
+  entry: [
+    'babel-polyfill',
+    './src'
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    moduleDirectories: ['node_modules']
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/ }
+    ]
   },
   devtool: 'eval-source-map',
   output: {
-    path: path.join(__dirname, 'build'),
+    path: buildPath,
     filename: '[name]-[chunkhash].js'
   },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin({ template: './src/index.html' })
   ],
   devServer: {
     inline: true
