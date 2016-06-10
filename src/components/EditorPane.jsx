247f4f702f475145f7c8d4ec41f2f0e7b54dc43d
Charles D'Angelo <c.g.dangelo@gmail.com>
2016-06-09 23:29:56 -0400

    Add editor, toolbar

--- src/components/EditorPane.jsx
+++ src/components/EditorPane.jsx
@@ -1,33 +1,123 @@
 import React, { Component } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import {
+  CompositeDecorator,
+  EditorState,
+  Entity,
+  Modifier,
+  convertToRaw,
+} from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
 
 import 'draft-js/dist/Draft.css';
 import styles from './EditorPane.css';
+
+const findIconEntities = (contentBlock, callback) => {
+  contentBlock.findEntityRanges((character) => {
+    const entityKey = character.getEntity();
+
+    return (
+      entityKey !== null &&
+      Entity.get(entityKey).getType() === 'icon'
+    );
+  }, callback);
+};
+
+const Icon = ({ entityKey }) => {
+  const { iconClass } = Entity.get(entityKey).getData();
+
+  return (
+    <img
+      alt={`{icon ${iconClass}}`}
+      className={iconClass}
+      style={{
+        height: '25px',
+        width: '25px',
+      }}
+    />
+  );
+};
 
 export default class EditorPane extends Component {
   constructor(props) {
     super(props);
 
-    this.handleChange = ::this.handleChange;
-  }
+    const decorator = new CompositeDecorator([
+      { strategy: findIconEntities, component: Icon },
+    ]);
 
-  state = {
-    editorState: EditorState.createEmpty(),
-  };
+    this.state = {
+      editorState: EditorState.createEmpty(decorator),
+    };
+
+    this.handleChange = ::this.handleChange;
+    this.insertBloodlust = ::this.insertBloodlust;
+    this.logRaw = ::this.logRaw;
+    this.logState = ::this.logState;
+  }
 
   handleChange(editorState) {
     this.setState({ editorState });
+  }
+
+  insertBloodlust() {
+    const { editorState } = this.state;
+    const contentState = editorState.getCurrentContent();
+    const selectionState = editorState.getSelection();
+
+    const entityKey = Entity.create('icon', 'IMMUTABLE', {
+      iconClass: 'spell_nature_bloodlust',
+    });
+
+    const iconReplacedContent = Modifier.replaceText(
+      contentState,
+      selectionState,
+      ' ',
+      null,
+      entityKey,
+    );
+
+    const newEditorState = EditorState.push(
+      editorState,
+      iconReplacedContent,
+      'insert-icon',
+    );
+
+    this.setState({
+      editorState: EditorState.forceSelection(
+        newEditorState,
+        iconReplacedContent.getSelectionAfter(),
+      ),
+    });
+  }
+
+  logRaw() {
+    console.log(convertToRaw(this.state.editorState.getCurrentContent()));
+  }
+
+  logState() {
+    console.log(this.state.editorState.toJS());
   }
 
   render() {
     const { editorState } = this.state;
 
     return (
-      <div className={styles.editorContainer}>
-        <Editor
-          editorState={editorState}
-          onChange={this.handleChange}
-        />
+      <div>
+        <div className={styles.debugButtons}>
+          <button onClick={this.logRaw}>Log Raw</button>
+          <button onClick={this.logState}>Log State</button>
+        </div>
+
+        <div className={styles.editorContainer}>
+          <div className={styles.editorToolbar}>
+            <button onClick={this.insertBloodlust}>Bloodlust</button>
+          </div>
+
+          <Editor
+            editorState={editorState}
+            onChange={this.handleChange}
+          />
+        </div>
       </div>
     );
   }
