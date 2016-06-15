b21fe6fb727e4bed6456dd2d2525db9f03763c93
Charles D'Angelo <c.g.dangelo@gmail.com>
2016-06-14 20:12:46 -0400

    Move common icons to icon component

--- src/components/EditorPane.jsx
+++ src/components/EditorPane.jsx
@@ -14,58 +14,10 @@ import addIcon from '../draft/modifiers/addIcon';
 import convertToAngry from '../draft/convertToAngry';
 import findEntitiesByType from '../draft/findEntitiesByType';
 import ButtonMenu from './ButtonMenu';
-import Icon from './Icon';
+import Icon, { commonIcons } from './Icon';
+import iconList from '../iconList';
 
 import styles from './EditorPane.scss';
-
-type IconMenu = Array<{
-  label: string,
-  icon: string,
-  outputsTo?: string,
-}>;
-
-const commonIcons: {
-  Haste: IconMenu,
-  Classes: IconMenu,
-  Roles: IconMenu,
-} = {
-  Haste: [
-    { label: 'Bloodlust', icon: 'spell_nature_bloodlust', outputsTo: '{bl}' },
-    { label: 'Heroism', icon: 'ability_shaman_heroism', outputsTo: '{hero}' },
-  ],
-
-  Classes: [
-    { label: 'Death Knight', icon: 'classicon_deathknight', outputsTo: '{deathknight}' },
-    { label: 'Demon Hunter', icon: 'classicon_demonhunter', outputsTo: '{demonhunter}' },
-    { label: 'Druid', icon: 'classicon_druid', outputsTo: '{druid}' },
-    { label: 'Hunter', icon: 'classicon_hunter', outputsTo: '{hunter}' },
-    { label: 'Mage', icon: 'classicon_mage', outputsTo: '{mage}' },
-    { label: 'Monk', icon: 'classicon_monk', outputsTo: '{monk}' },
-    { label: 'Paladin', icon: 'classicon_paladin', outputsTo: '{paladin}' },
-    { label: 'Priest', icon: 'classicon_priest', outputsTo: '{priest}' },
-    { label: 'Rogue', icon: 'classicon_rogue', outputsTo: '{rogue}' },
-    { label: 'Shaman', icon: 'classicon_shaman', outputsTo: '{shaman}' },
-    { label: 'Warlock', icon: 'classicon_warlock', outputsTo: '{warlock}' },
-    { label: 'Warrior', icon: 'classicon_warrior', outputsTo: '{warrior}' },
-  ],
-
-  Markers: [
-    { label: 'Star', icon: 'iconsmall_raidstar', outputsTo: '{star}' },
-    { label: 'Circle', icon: 'iconsmall_raidcircle', outputsTo: '{circle}' },
-    { label: 'Diamond', icon: 'iconsmall_raiddiamond', outputsTo: '{diamond}' },
-    { label: 'Triangle', icon: 'iconsmall_raidtriangle', outputsTo: '{triangle}' },
-    { label: 'Moon', icon: 'iconsmall_raidmoon', outputsTo: '{moon}' },
-    { label: 'Square', icon: 'iconsmall_raidsquare', outputsTo: '{square}' },
-    { label: 'Cross', icon: 'iconsmall_raidcross', outputsTo: '{cross}' },
-    { label: 'Skull', icon: 'iconsmall_raidskull', outputsTo: '{skull}' },
-  ],
-
-  Roles: [
-    { label: 'Tank', icon: 'iconsmall_roletank', outputsTo: '{tank}' },
-    { label: 'Healer', icon: 'iconsmall_rolehealer', outputsTo: '{heal}' },
-    { label: 'DPS', icon: 'iconsmall_roledps', outputsTo: '{dps}' },
-  ],
-};
 
 class EditorPane extends Component {
   constructor(props: any) {
